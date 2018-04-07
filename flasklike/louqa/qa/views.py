c1294759c19c8555184b3821b08e87e95a4a7557
LCClcc <lichengcai22@gmail.com>
2018-04-08 01:05:50 +0800

    Update views.py

--- flasklike/louqa/qa/views.py
+++ flasklike/louqa/qa/views.py
@@ -1,12 +1,14 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Blueprint
-from flask import render_template
+from flask import Blueprint, render_template
+from ..user import User
 
 qa = Blueprint('qa', __name__, url_prefix='')
+
 
 @qa.route('/<title>')
 @qa.route('/', defaults={'title': None})
 def index(title):
-	return render_template("qa/index.html", title=title, tem_str="world")
+      user = User.query.filter().first()
+      return render_template("qa/index.html", title=title, tem_str=user.name)
