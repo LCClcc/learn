181b9b609bf7f9fec0bd4b8220049921edcba4d9
lcc <lcc123@pku.edu.cn>
2018-04-08 00:22:19 +0800

    add title

--- flasklike/louqa/qa/views.py
+++ flasklike/louqa/qa/views.py
@@ -2,9 +2,11 @@
 # encoding: utf-8
 
 from flask import Blueprint
+from flask import render_template
 
 qa = Blueprint('qa', __name__, url_prefix='')
 
-@qa.route('/')
-def index():
-	return "<p>Hellw World</p>"
+@qa.route('/<title>')
+@qa.route('/', defaults={'title': None})
+def index(title):
+	return render_template("qa/index.html", title=title, tem_str="world")
